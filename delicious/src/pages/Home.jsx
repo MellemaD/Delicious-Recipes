128be274cf0e2fe927b8c02a67eccf3c56fa318c
MellemaD <68898788+MellemaD@users.noreply.github.com>
2022-12-24 16:07:23 +0100

    Finished project

--- delicious/src/pages/Home.jsx
+++ delicious/src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
+import {motion} from "framer-motion";
 
 
 import React from 'react'
 
 function Home() {
     return (
-        <div>
+        /*motion div here and in cuisine (and the rest) for animation purposes*/
+        // Only fades in, not out so need to wrap [AnimatePresence] around pages
+        // But since working with routes it will need location
+        <motion.div
+            animate={{opacity: 1}}
+            initial={{opacity: 0}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+        >
             <Veggie/>
             <Popular/>
-        </div>
+        </motion.div>
 
     )
 }
--- delicious/src/pages/Pages.jsx
+++ delicious/src/pages/Pages.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import Home from './Home';
 import Cuisine from './Cuisine';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes, useLocation} from 'react-router-dom';
+import Searched from "./Searched";
+import Recipe from "./Recipe";
+import {AnimatePresence} from "framer-motion";
 
 
 
 function Pages() {
+    const location = useLocation();
     return (
-        // <BrowserRouter> {/* Gives ability for other routes to work */}
-        <Routes> {/* is intelligent component that looks at the paths and figures out which to render */}
-            <Route path="/" element={<Home/>} />
-            <Route path="/cuisine/:type" element={<Cuisine/>} />
-
-        </Routes>
-        // Move to app </BrowserRouter>
+        // Explanation for animatepresence and location in Home.js
+        <AnimatePresence>
+            {/*  <BrowserRouter> Gives ability for other routes to work */}
+            <Routes location={location} key={location.pathname}> {/* is intelligent component that looks at the paths and figures out which to render */}
+                <Route path="/" element={<Home/>} />
+                <Route path="/cuisine/:type" element={<Cuisine/>} />
+                <Route path="/searched/:search" element={<Searched/>} />
+                <Route path="/recipe/:name" element={<Recipe/>} />
+            </Routes>
+        </AnimatePresence>
+        /*/ Move to app </BrowserRouter>/*/
     )
 }
 
